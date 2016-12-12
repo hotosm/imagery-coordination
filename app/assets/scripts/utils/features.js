f622b8cb18bbbd23e8f2d4ff94eeed6d508ee52c
Nick Bumbarger <nbumbarger@gmail.com>
2016-12-12 16:29:51 -0500

    Add loading of saved results to task edit view

--- app/assets/scripts/utils/features.js
+++ app/assets/scripts/utils/features.js
@@ -1,25 +1,35 @@
 export function geometryToFeature (results) {
   if (results.length > 1) {
+    if (results[0].geometry) {
+      return {
+        'type': 'FeatureCollection',
+        'features':
+          results.map((result) => {
+            return {
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Polygon',
+                'coordinates': [result.geometry]
+              },
+              'properties': { }
+            };
+          })
+      };
+    }
     return {
-      'type': 'FeatureCollection',
-      'features':
-        results.map((result) => {
-          return {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [result.geometry]
-            },
-            'properties': { }
-          };
-        })
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': [results]
+      },
+      'properties': { }
     };
   } else if (results.length) {
     return {
       'type': 'Feature',
       'geometry': {
         'type': 'Polygon',
-        'coordinates': [results[0].geometry]
+        'coordinates': results[0].geometry
       },
       'properties': { }
     };
