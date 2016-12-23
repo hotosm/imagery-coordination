be60ba2db2f71adbb964359e5b7615b1e9715970
Daniel da Silva <daniel@developmentseed.org>
2016-12-23 14:05:25 +0000

    Fix logo path

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -13,7 +13,7 @@ var PageFooter = React.createClass({
         <div className='inner'>
 
           <div className='col--main'>
-            <img src='/assets/graphics/layout/oam-logo-v-pos.svg' alt='OpenAerialMap logo' className='oam-logo'/>
+            <img src='assets/graphics/layout/oam-logo-v-pos.svg' alt='OpenAerialMap logo' className='oam-logo'/>
             <ul className='related'>
               <li className='related__item related__item--upload'><a href='https://upload.openaerialmap.org/' title='Visit OAM Uploader'>Upload Imagery</a></li>
               <li className='related__item related__item--browse'><a href='https://beta.openaerialmap.org/' title='Visit OAM Browser'>Browse Imagery</a></li>
