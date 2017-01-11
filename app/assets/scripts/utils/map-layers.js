94fb0166f238129fe6905072218501f88d9d7817
Daniel da Silva <daniel@developmentseed.org>
2017-01-11 11:35:28 +0000

    Add bing and DG base layers.
    Fix #62. Fix #63

--- app/assets/scripts/utils/map-layers.js
+++ app/assets/scripts/utils/map-layers.js
@@ -2,18 +2,21 @@
 
 module.exports = [
   {
-    id: 'oam-base',
-    name: 'Mapbox Light',
-    url: 'https://api.mapbox.com/styles/v1/hot/cividbt4w00ax2jn8517i2nc9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaG90IiwiYSI6ImNpdmlkM2lkMDAwYTAydXBnNXFkd2EwemsifQ.KPrUb_mKlPmHCR6LNrSihQ'
-  },
-  {
-    id: 'osm',
-    name: 'OpenStreetMap (Standard)',
-    url: 'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
-  },
-  {
     id: 'satellite',
+    type: 'raster',
     name: 'Mapbox Satellite',
     url: 'https://api.mapbox.com/styles/v1/hot/civicyccw00bv2io77zqq401h/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaG90IiwiYSI6ImNpdmlkM2lkMDAwYTAydXBnNXFkd2EwemsifQ.KPrUb_mKlPmHCR6LNrSihQ'
+  },
+  {
+    id: 'digital-globe',
+    type: 'raster',
+    name: 'Digital Globe',
+    url: 'https://api.mapbox.com/styles/v1/digitalglobe/ciode6t5k0081aqm7k06dod4v/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpdHZtM3FjbzAwNjYyem40c3FlNG11eGkifQ.dxTyi-e8nUFGtGQON78xQA'
+  },
+  {
+    id: 'bing-satellite',
+    type: 'raster',
+    name: 'Bing Satellite',
+    url: 'http://t1.tiles.virtualearth.net/tiles/a{quadkey}.jpeg?g=1398'
   }
 ];
