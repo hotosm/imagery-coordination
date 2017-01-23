fbdf4eea4851870d29a868c837b9a76500f7fd04
Daniel da Silva <daniel@developmentseed.org>
2017-01-23 13:41:56 +0000

    Remove create request button from form

--- app/assets/scripts/views/request-form.js
+++ app/assets/scripts/views/request-form.js
@@ -262,7 +262,7 @@ This action is permanent, and all associated tasks will be deleted as well.`;
                 value={this.state.data.notes} onChange={this.onFieldChange.bind(null, 'notes')} ></textarea>
           </div>
           <div className='form__actions'>
-            <button type='submit' className={'button button--primary'} onClick={this.onSave}><span>{editing ? 'Save request' : 'Create request'}</span></button>
+            {/* <button type='submit' className={'button button--primary'} onClick={this.onSave}><span>{editing ? 'Save request' : 'Create request'}</span></button> */}
             <button type='submit' className={'button button--secondary'} onClick={this.onSaveAndAdd}><span>{editing ? 'Save & add task' : 'Create & add task'}</span></button>
             <Link to={editing ? `requests/${this.props.params.reqid}` : '/'} className={'button button--base'}><span>Cancel</span></Link>
           </div>
