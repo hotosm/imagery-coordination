6a4ad441dacac2df13cce3064637a42b0ef129b3
sharkinsspatial <sharkinsgis@gmail.com>
2017-08-04 08:59:15 -0300

    Include all tests in main test.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 /* global window setTimeout */
 /* eslint-disable import/imports-first, no-unused-vars */
 import test from 'tape';
-import imagerySearchReducerTest from
-    './reducers/imagery-search-test';
-import addRequestLinkTest from
-    './components/add-request-link-test';
+import imagerySearchReducerTest from './reducers/imagery-search-test';
+import addRequestLinkTest from './components/add-request-link-test';
+import editMapTest from './components/edit-map-test';
+import mapReducerTest from './reducers/map-reducer-test';
 
 test('Shutdown', (t) => {
   t.pass('Shutting down');
