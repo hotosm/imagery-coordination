5f8643129a6858402f43cefdcefb412da02ecf3d
Nate Smith <nateasmith@gmail.com>
2017-08-11 11:08:04 +0100

    Update staging MB token

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -7,7 +7,7 @@ module.exports = {
   environment: 'staging',
   auth0Client: 'xD5HP0K0aHMZdZjFCpC01XtIocAMYyel',
   auth0Domain: 'smit1678.auth0.com',
-  mbToken: 'pk.eyJ1Ijoib3BlbmFlcmlhbG1hcCIsImEiOiJjaXl4MXkyZDMwMDB0MnhwN3c5b2I5MmtqIn0.-NsKzzY8CAFdKVAXqtC9Tg',
+  mbToken: 'pk.eyJ1IjoiaG90IiwiYSI6ImNpeXEwbHF2aTAwMDcycW10dGs4Z28wdGYifQ.p9gO4DrmIw1zBIKhvtIivw',
 
   api: 'https://imagery-coord-api-staging.herokuapp.com'
 };
