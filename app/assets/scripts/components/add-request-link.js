2ce6ac672f337064d927dc7aa44451f433a2029d
sharkinsspatial <sharkinsgis@gmail.com>
2017-10-02 12:00:31 -0300

    Update Create Request to be enabled only after user initiated map movement.  Closes #111.

--- app/assets/scripts/components/add-request-link.js
+++ app/assets/scripts/components/add-request-link.js
@@ -4,16 +4,17 @@ import { Link } from 'react-router';
 import _ from 'lodash';
 import { isLoggedIn } from '../utils/auth-service';
 
-const AddRequestLink = (props) => {
-  if (props.coordinator) {
-    return <Link to='/requests/edit' className='button-skip'>Skip this step</Link>;
+export const AddRequestLink = (props) => {
+  if (props.coordinator && props.createRequestEnabled) {
+    return <Link to='/requests/edit' className='button-add-request'>Complete request</Link>;
   } else {
     return <div></div>;
   }
 };
 
 AddRequestLink.propTypes = {
-  coordinator: T.bool.isRequired
+  coordinator: T.bool.isRequired,
+  createRequestEnabled: T.bool.isRequired
 };
 
 export function isCoordinator (token, user) {
@@ -22,7 +23,8 @@ export function isCoordinator (token, user) {
 }
 
 const mapStateToProps = state => ({
-  coordinator: isCoordinator(state.user.token, state.user)
+  coordinator: isCoordinator(state.user.token, state.user),
+  createRequestEnabled: state.imagerySearch.createRequestEnabled
 });
 
 export default connect(mapStateToProps, null)(AddRequestLink);
--- test/components/add-request-link-test.js
+++ test/components/add-request-link-test.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import test from 'tape';
-import { isCoordinator, __RewireAPI__ as RewireAddLink } from
+import { shallow } from 'enzyme';
+import { Link } from 'react-router';
+import { AddRequestLink, isCoordinator, __RewireAPI__ as RewireAddLink } from
   '../../app/assets/scripts/components/add-request-link';
 
 test('isCoordinator is true when user is logged in and is a coordinator', t => {
@@ -32,5 +35,23 @@ test('isCoordinator is false when user is not a coordinator', t => {
   };
   const coordinator = isCoordinator(1, user);
   t.notOk(coordinator);
+  RewireAddLink.__ResetDependency__('isLoggedIn');
+});
+
+test('Returns empty div when createRequestEnabled is false', t => {
+  RewireAddLink.__Rewire__('isLoggedIn', () => true);
+  t.plan(1);
+  const wrapper = shallow(<AddRequestLink coordinator={true}
+                            createRequestEnabled={false} />);
+  t.equal(wrapper.find(Link).length, 0);
+  RewireAddLink.__ResetDependency__('isLoggedIn');
+});
+
+test('Returns Link when createRequestEnabled is true', t => {
+  RewireAddLink.__Rewire__('isLoggedIn', () => true);
+  t.plan(1);
+  const wrapper = shallow(<AddRequestLink coordinator={true}
+                            createRequestEnabled={true} />);
+  t.equal(wrapper.find(Link).length, 1);
   RewireAddLink.__ResetDependency__('isLoggedIn');
 });
